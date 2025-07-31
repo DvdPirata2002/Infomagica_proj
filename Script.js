15e5662091e962c9e82c1adb28fd60bee26c5b84
DvdPirata <davivida1230908@gmail.com>
2025-07-31 16:29:25 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,35 @@
+
+var traco = document.getElementById('xburguer')
+var menu = document.querySelector('.menu')
+var body = document.body;
+var drk = document.getElementById('drkToggle')
+
+// Verifica o estado do modo escuro ao carregar a página
+
+window.onload = function() {
+    if (localStorage.getItem('modoEscuro') === 'true') {
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+    }
+};
+
+// expande o menu
+
+traco.addEventListener('click', function() {
+menu.classList.toggle('grande')
+
+})
+
+// faz o modo escuro funcionar
+
+drk.addEventListener('click', function(){
+body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('modoEscuro', 'true');
+    } else {
+        localStorage.setItem('modoEscuro', 'false');
+    }
+
+});
